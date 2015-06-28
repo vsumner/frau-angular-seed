3ed82fd0eeb2fbe90e7cead876b4273f50cd0625
Victor Sumner <victor.sumner@desire2learn.com>
2015-06-27 21:48:19 -0400

    added router

---
+++ src/js/app.routes.js
@@ -0,0 +1,20 @@
+var angular = require('angular'),
+    config = require('config');
+
+angular.module(config.name)
+.config([
+    '$locationProvider',
+    '$routeProvider',
+    function($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+      // routes
+      $routeProvider
+        .when("/", {
+          templateUrl: "./partials/partial1.html",
+          controller: "MainController"
+        })
+        .otherwise({
+           redirectTo: '/'
+        });
+    }
+  ])
