2109a05e641364cc37e3a31045da14e77edf56a0
Victor Sumner <victor.sumner@d2l.com>
2015-06-29 11:26:10 -0400

    added ngResource

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@
 var angular = require('angular'),
     config = require('./config');
 
-angular.module( config.name, ['ngRoute']);
+angular.module( config.name, ['ngRoute','ngResource']);
 // configuration
 require('./app.routes');
